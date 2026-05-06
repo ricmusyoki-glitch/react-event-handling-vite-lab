fd132e3480e6f0add73eb1a0fa1ffbfd0fbc5126
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-05-06 21:14:49 +0300

    Created a password input event

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,8 +1,15 @@
 // Code PasswordInput Component Here
 
 function PasswordInput (){
+    const handleChange = () => {
+    console.log("Entering password...");
+  };
     return(
-        <></>
+        <input
+      type="password"
+      placeholder="Enter Password"
+      onChange={handleChange}
+    />
     )
 }
 
